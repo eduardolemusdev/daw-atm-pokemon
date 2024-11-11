3bf5ac06ba6a17abb23d00f1cf483891425f0443
Eduardo Lemus <eduardolemus3098@gmail.com>
2024-11-10 18:52:59 -0600

    feat: dinamic graph about transactions amount by type

--- src/js/services/chartsStatsServices.js
+++ src/js/services/chartsStatsServices.js
@@ -1,4 +1,5 @@
 const redirectAtmMenuBtn = $("#redirectAtmMenuBtn");
+const currentUser = checkAuthUser();
 
 redirectAtmMenuBtn.addEventListener("click", () => {
   redirectTomeAtmMenu();
@@ -33,14 +34,52 @@ new Chart(pieChartServices, {
   },
 });
 
+//primero obtenemos los tipos de transacion unicos que tiene el usuario,
+// ["deposit", "deposit", "deposit", "withdraw", "withdraw"]
+// => ["deposit", "withdraw"]
+const transactionTypes = Array.from(
+  new Set(
+    currentUser.transactionHistory.map(
+      (transaction) => transaction.transactionType
+    )
+  )
+).map((type) => {
+  return {
+    type,
+    totalAmount: 0,
+  };
+});
+
+let chartData = [];
+currentUser.transactionHistory.forEach((transaction) => {
+  // Encontrar el tipo de transacción correspondiente en el arreglo transactionTypes
+  const typeObj = transactionTypes.find(
+    (t) => t.type === transaction.transactionType
+  );
+
+  const restOfTypes = transactionTypes.filter(
+    (t) => t.type !== transaction.transactionType
+  );
+
+  // Sumar el amount de la transacción al totalAmount del tipo correspondiente
+  if (typeObj) {
+    typeObj.totalAmount += parseFloat(transaction.amount.replace("$", ""));
+  }
+
+  chartData = [...restOfTypes, typeObj];
+});
+
+console.log(chartData);
+
 new Chart(barChartIncomeTransacctions, {
   type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: chartData.map((item) => item.type),
     datasets: [
       {
         label: "# Transacciones entrantes por mes",
-        data: [12, 19, 3, 5, 2, 3],
+        data: chartData.map((item) => item.totalAmount),
+
         borderWidth: 5,
         fill: true,
         backgroundColor: "rgba(13, 163, 28, 0.459)",
